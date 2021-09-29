44997bdb3bff0e4211c3bb7bbf8307882f8a0ff9
vijaynandan <72315460+vijaynandan@users.noreply.github.com>
2021-09-29 18:32:46 +0530

    Add files via upload

---
+++ Left-Right-pattern.py
@@ -0,0 +1,24 @@
+def pattern(n):
+    for i in range(n,0,-1):
+        for j in range(i,n):
+            print(" ",end=" ")
+        for j in range(n):
+            print("-", end=" ") 
+        for i in range(j):
+            print("#", end=" ") 
+        for i in range(j+1):
+            print("+", end=" ")    
+        print()
+    for i in range(1,n):
+        for j in range(i,n):
+            print(" ",end=" ")
+        for j in range(n):
+            print("-", end=" ") 
+        for i in range(j):
+            print("#", end=" ") 
+        for i in range(j+1):
+            print("+", end=" ")    
+        print()
+    
+n=5
+pattern(n)
